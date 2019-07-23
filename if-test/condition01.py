604ac206852c2ffbfb17f4512d299db44b545561
Ali G <alighazanfar89@gmail.com>
2019-07-23 15:06:34 -0400

    If-Test

---
+++ if-test/condition01.py
@@ -0,0 +1,11 @@
+#!usr/bin/env python3
+
+
+hostname = input("What value should we set for hostname? ")
+if hostname.lower() == "mtg":
+	print("Hostname found to be MTG")
+	print("Hostname matches expected config")
+
+#Always print this
+print ("Exiting the Script")
+
