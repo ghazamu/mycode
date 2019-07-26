48b23991c67f377686797f542567677b03dbb038
Ali G <alighazanfar89@gmail.com>
2019-07-26 13:57:42 -0400

    more code

---
+++ marvel/charc.py
@@ -0,0 +1,43 @@
+import hashlib
+import requests
+import datetime
+
+from pprint import pprint as pp
+
+timestamp = datetime.datetime.now().strftime('%Y-%m-%d%H:%M:%S')
+pub_key = '334df4cf3040a3723167ff89ba156a42'
+priv_key = '82a66b2a7a5670869de6684023931a6bc88a89ab'
+
+
+def hash_params():
+    """ Marvel API requires server side API calls to include
+    md5 hash of timestamp + public key + private key """
+
+    hash_md5 = hashlib.md5()
+    hash_md5.update(f'{timestamp}{priv_key}{pub_key}'.encode('utf-8'))
+    hashed_params = hash_md5.hexdigest()
+
+    return hashed_params
+
+params = {'ts': timestamp, 'apikey': pub_key, 'hash': hash_params()};
+res = requests.get('https://gateway.marvel.com:443/v1/public/characters',
+                   params=params)
+
+results = res.json()
+result2 = results.get("data").get("results")
+print("\n")
+print("\n")
+#results = len(results.get("data").get("results"))
+i = 0
+j = 0
+
+
+for i in range(len(result2)):
+    #print(i)
+    print(str(i) + ": Character: " + result2[i].get("name"))
+    print("\n***Apperences***")
+    for j in range(len(result2[i].get("comics").get("items"))):
+        print(result2[i].get("comics").get("items")[j].get("name"))
+    #print(result2[i].get("comics").get("items"))
+    print("\n")
+
